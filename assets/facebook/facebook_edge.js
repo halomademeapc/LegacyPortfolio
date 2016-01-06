c852e3c3340094613d981a40c03add661db0fadb
halomademeapc <opr9029@gmail.com>
2016-01-06 14:28:11 -0500

    Wipe and Rebuild

---
+++ assets/facebook/facebook_edge.js
@@ -0,0 +1,19 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x2='5.0.0',x18='rgba(0,0,0,0)',zx='scaleX',x10='rgba(229,46,41,0.00)',x='text',m='rect',x14='0px',e12='${Symbol_12}',i='none',x3='6.0.0.400',p='px',lf='left',x17='facebook',x7='Arial, Helvetica, sans-serif',l='normal',x9='break-word',x16='auto',e13='${Text}',g='image',fs='font-size',x15='160px',x11='rgba(255,255,255,1)',x6='14',x4='rgba(81,96,157,1.00)',zy='scaleY',h='height',x8='200',w='width',tp='top',xc='rgba(0,0,0,1)';var g19='facebook.svg';var s5="<p style=\"margin: 0px; font-family: Arial, Helvetica, sans-serif; font-weight: 200; font-style: normal; text-decoration: none; font-size: 8px; color: rgb(255, 255, 255); background-color: rgba(0, 0, 0, 0); letter-spacing: 0px; text-transform: none; word-spacing: 0px; text-align: left; text-indent: 0px; line-height: normal;\">Facebook</p><p style=\"margin: 0px;\"><span style=\"font-weight: 900; color: rgb(255, 255, 255);\">Alex Griffith</span></p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Rectangle2',t:m,r:['0px','0px','160px','160px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]},{id:'Text',t:x,r:['56px','68px','160px','14px','auto','auto'],text:s5,align:"left",n:[x7,[x6,p],xc,x8,i,"",x9,l]},{id:'Symbol_12',symbolName:'Symbol_1',t:m,r:['-43px','0px','160px','160px','auto','auto'],tf:[[],[],[],['0.2325','1.00188']]},{id:'Actions',t:m,r:['0px','0px','160px','160px','auto','auto'],f:[x10],s:[0,"rgb(0, 0, 0)",i]}],style:{'${Stage}':{isStage:true,r:['null','null','160px','160px','auto','auto'],overflow:'hidden',f:[x11]}}},tt:{d:380,a:y,data:[["eid56",tp,0,375,"easeInOutBack",e12,'0px','0px'],["eid48",w,375,0,"easeInOutBack",e12,'160px','160px'],["eid49",zx,0,375,"easeInOutBack",e12,'0.2325','1'],["eid25",lf,0,375,"easeInOutBack",e13,'56px','160px'],["eid33",w,375,0,"easeInOutBack",e13,'160px','160px'],["eid47",h,375,0,"easeInOutBack",e12,'160px','160px'],["eid51",lf,0,375,"easeInOutBack",e12,'-43px','0px'],["eid57",zy,0,375,"easeInOutBack",e12,'0.2325','1.00188'],["eid58",h,375,5,"easeInOutBack",e13,'14px','31px'],["eid39",fs,0,0,"easeInOutBack",e13,'14px','14px']]}},"Symbol_1":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{r:[x14,x14,x15,x15,x16,x16],id:x17,t:g,f:[x18,im+g19,x14,x14]}],style:{'${symbolSelector}':{r:[_,_,x15,x15]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-8287664");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${Actions}","mouseover",function(sym,e){sym.playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Actions}","mouseout",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Actions}","click",function(sym,e){window.open("https://www.facebook.com/halomademeapc","_blank");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-8287664");
