d122a04a77303ae97884b4efd56b38b3c71df1d2
halomademeapc <opr9029@gmail.com>
2016-01-15 15:58:28 -0500

    more social, photos, and rendar

---
+++ assets/xbl/xbl_edge.js
@@ -0,0 +1,19 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x2='5.0.0',x20='rgba(0,0,0,0)',zx='scaleX',x10='rgba(229,46,41,0.00)',x14='Xbox_one_logo_1',x16='6px',x15='7px',x='text',m='rect',x22='0px',e12='${Symbol_12}',i='none',x3='6.0.0.400',p='px',lf='left',x17='146px',x24='160px',x23='true',x7='Arial, Helvetica, sans-serif',x9='break-word',l='normal',xc='rgba(0,0,0,1)',x19='auto',e13='${Text}',g='image',fs='font-size',zy='scaleY',x11='rgba(255,255,255,1)',x4='rgba(126,185,0,1.00)',h='height',x18='148px',x8='200',w='width',tp='top',x6='14';var g21='Xbox_one_logo%20%281%29.svg';var s5="<p style=\"margin: 0px; font-family: Arial, Helvetica, sans-serif; font-weight: 200; font-style: normal; text-decoration: none; font-size: 8px; color: rgb(255, 255, 255); background-color: rgba(0, 0, 0, 0); letter-spacing: 0px; text-transform: none; word-spacing: 0px; text-align: left; text-indent: 0px; line-height: normal;\">Xbox Live</p><p style=\"margin: 0px;\"><span style=\"font-weight: 900; color: rgb(255, 255, 255); font-size: 11px;\">halomademeapc</span></p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Rectangle2',t:m,r:['0px','0px','160px','160px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]},{id:'Text',t:x,r:['56px','68px','160px','14px','auto','auto'],text:s5,align:"left",n:[x7,[x6,p],xc,x8,i,"",x9,l]},{id:'Symbol_12',symbolName:'Symbol_1',t:m,r:['-43px','0px','160px','160px','auto','auto'],tf:[[],[],[],['0.2325','1.00188']]},{id:'Actions',t:m,r:['0px','0px','160px','160px','auto','auto'],f:[x10],s:[0,"rgb(0, 0, 0)",i]}],style:{'${Stage}':{isStage:true,r:['null','null','160px','160px','auto','auto'],overflow:'hidden',f:[x11]}}},tt:{d:380,a:y,data:[["eid56",tp,0,375,"easeInOutBack",e12,'0px','0px'],["eid48",w,375,0,"easeInOutBack",e12,'160px','160px'],["eid49",zx,0,375,"easeInOutBack",e12,'0.2325','1'],["eid25",lf,0,375,"easeInOutBack",e13,'56px','160px'],["eid33",w,375,0,"easeInOutBack",e13,'160px','160px'],["eid47",h,375,0,"easeInOutBack",e12,'160px','160px'],["eid51",lf,0,375,"easeInOutBack",e12,'-43px','0px'],["eid57",zy,0,375,"easeInOutBack",e12,'0.2325','1.00188'],["eid58",h,375,5,"easeInOutBack",e13,'14px','31px'],["eid39",fs,0,0,"easeInOutBack",e13,'14px','14px']]}},"Symbol_1":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x14,t:g,r:[x15,x16,x17,x18,x19,x19],f:[x20,im+g21,x22,x22]}],style:{'${symbolSelector}':{isStage:x23,r:[undefined,undefined,x24,x24]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-8287664");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${Actions}","mouseover",function(sym,e){sym.playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Actions}","mouseout",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Actions}","click",function(sym,e){window.open("http://live.xbox.com/en-US/Profile?gamertag=halomademeapc","_blank");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-8287664");
