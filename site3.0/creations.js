e475c44135f03e42fc4407219fc92c66f586caf0
halomademeapc <opr9029@gmail.com>
2016-09-29 22:40:41 -0400

    creation blades

---
+++ site3.0/creations.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+$('.blade').hover(
+	function() {
+		$(this).animate({width: '100%',
+		letterSpacing: '.1em'},200);
+		
+	},
+	function() {
+		$(this).animate({width: '95%',
+		letterSpacing: '0'},200);
+		
+	});
