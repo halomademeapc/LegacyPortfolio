a770c9290bcd98bf609bbc49d65b03b71fecc7c7
Alex Griffith <opr9029@gmail.com>
2017-01-19 22:57:20 -0500

    site3.0 deployment

---
+++ legacy/assets/twitter/Twitter_edge.js
@@ -0,0 +1,19 @@
+
+(function(compId){var _=null,y=true,n=false,x9='break-word',x18='rgba(0,0,0,0)',zx='scaleX',x10='rgba(229,46,41,0.00)',x21='true',x15='0',x='text',m='rect',x16='160px',e12='${Symbol_12}',i='none',x4='rgba(95,169,221,1.00)',x3='6.0.0.400',x14='twitter',rt='right',lf='left',x20='0px',x7='Arial, Helvetica, sans-serif',l='normal',x1='6.0.0',x17='auto',e13='${Text}',x2='5.0.0',p='px',xc='rgba(0,0,0,1)',x11='rgba(255,255,255,1)',x6='14',g='image',zy='scaleY',fs='font-size',x8='200',h='height',w='width',tp='top';var g19='twitter.svg';var s5="<p style=\"margin: 0px; font-family: Arial, Helvetica, sans-serif; font-weight: 200; font-style: normal; text-decoration: none; font-size: 8px; color: rgb(255, 255, 255); background-color: rgba(0, 0, 0, 0); letter-spacing: 0px; text-transform: none; word-spacing: 0px; text-align: left; text-indent: 0px; line-height: normal;\">Twitter</p><p style=\"margin: 0px;\"><span style=\"font-weight: 900; color: rgb(255, 255, 255); font-size: 11px;\">@halomademeapc</span></p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Rectangle2',t:m,r:['0px','0px','160px','160px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]},{id:'Text',t:x,r:['47px','68px','160px','14px','auto','auto'],text:s5,align:"left",n:[x7,[x6,p],xc,x8,i,"",x9,l]},{id:'Symbol_12',symbolName:'Symbol_1',t:m,r:['-57px','0px','160px','160px','auto','auto'],tf:[[],[],[],['0.2325','1.00188']]},{id:'Actions',t:m,r:['0px','0px','160px','160px','auto','auto'],f:[x10],s:[0,"rgb(0, 0, 0)",i]}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'160px','160px'],overflow:'hidden',f:[x11]}}},tt:{d:380,a:y,data:[["eid56",tp,0,375,"easeInOutBack",e12,'0px','0px'],["eid48",w,375,0,"easeInOutBack",e12,'160px','160px'],["eid70",rt,0,0,"easeInOutBack",e12,'auto','auto'],["eid49",zx,0,375,"easeInOutBack",e12,'0.2325','1'],["eid63",tp,0,0,"easeInOutBack",e13,'68px','68px'],["eid33",w,375,0,"easeInOutBack",e13,'160px','160px'],["eid51",lf,0,375,"easeInOutBack",e12,'-57px','0px'],["eid47",h,375,0,"easeInOutBack",e12,'160px','160px'],["eid25",lf,0,375,"easeInOutBack",e13,'47px','160px'],["eid39",fs,0,0,"easeInOutBack",e13,'14px','14px'],["eid57",zy,0,375,"easeInOutBack",e12,'0.2325','1.00188'],["eid58",h,375,5,"easeInOutBack",e13,'14px','31px']]}},"Symbol_1":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x14,t:g,r:[x15,x15,x16,x16,x17,x17],f:[x18,im+g19,x20,x20]}],style:{'${symbolSelector}':{isStage:x21,r:[undefined,undefined,x16,x16]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-8287664");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${Actions}","mouseover",function(sym,e){sym.playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Actions}","mouseout",function(sym,e){sym.play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Actions}","click",function(sym,e){window.open("https://twitter.com/halomademeapc","_blank");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-8287664");
