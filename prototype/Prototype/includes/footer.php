e30f208aa40fca2aa55e80ef0e1693fde91c321c
halomademeapc <opr9029@gmail.com>
2017-04-01 12:08:47 -0400

    prototype v2

---
+++ prototype/Prototype/includes/footer.php
@@ -0,0 +1,12 @@
+<!-- BEGIN FOOTER -->    
+<div id="footer">
+   
+	<ul id="footerNav">
+		<li><a href="home.php" target="_self"><img src="images/return.png" /></a></li>
+        <li id="map"></li>
+        <li><a href="index.php" target="_self"><img src="images/logo.png" /></a></li>
+        <li><img src="images/logoshadow.png" /></li>
+	</ul>
+    
+</div>	   
+<!-- END FOOTER -->
