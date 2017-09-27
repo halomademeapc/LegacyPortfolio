94cc8c1faef54d6ea21b0358f32f0f22a959484e
Alex Griffith <halomademeapc@users.noreply.github.com>
2017-09-27 14:12:00 -0400

    added responsive css and mobile menu

--- common.js
+++ common.js
@@ -2,4 +2,9 @@
 
 //create navbar
 $("#topnavbar").load("navbar.html");
+$("#mobilemenu").load("mobilemenu.html");
 $("#topnavbar").addClass("borderXwidth navbarback");
+
+$('#burger').click(function () {
+    $('#mobilemenu').slideToggle();
+});
