88c6522948325e29d2c45767fb2f5ac6479c7088
Alex Griffith <halomademeapc@users.noreply.github.com>
2017-09-27 15:52:40 -0400

    add mobile css to creations page

--- creations.js
+++ creations.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-
+/*
 $('.blade').hover(
 	function() {
 		$(this).animate({width: '100%',
@@ -10,4 +10,4 @@ $('.blade').hover(
 		$(this).animate({width: '95%',
 		letterSpacing: '0'},200);
 		
-	});
+	});*/
