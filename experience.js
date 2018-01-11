98ecc85431cd7029eae48173407085ca790c2956
Alex Griffith <halomademeapc@users.noreply.github.com>
2018-01-11 16:02:24 -0500

    implement two-phase toggling on experience filters

--- experience.js
+++ experience.js
@@ -1,25 +1,65 @@
 // JavaScript Document
 
 $('#alltoggle').click(function() {
-	$('.course').slideDown();
-	$('.work').slideDown();
-	$('.volunteer').slideDown();
+	enableVisibility($(".course,.work,.volunteer"));
+	//$('.course').slideDown();
+	//$('.work').slideDown();
+	//$('.volunteer').slideDown();
+	updateVisiblity($(".course,.work,.volunteer"));
 });
 
 $('#worktoggle').click(function() {
-	$('.course').slideUp();
-	$('.volunteer').slideUp();
-	$('.work').slideDown();
+	disableVisibility($(".course,.volunteer"));
+	enableVisibility($(".work"));
+	//$('.course').slideUp();
+	//$('.volunteer').slideUp();
+	//$('.work').slideDown();
+	updateVisiblity($(".course,.work,.volunteer"));
 });
 
 $('#schooltoggle').click(function() {
-	$('.work').slideUp();
-	$('.volunteer').slideUp();
-	$('.course').slideDown();
+	disableVisibility($(".work,.volunteer"));
+	enableVisibility($(".course"));
+	//$('.work').slideUp();
+	//$('.volunteer').slideUp();
+	//$('.course').slideDown();
+	updateVisiblity($(".course,.work,.volunteer"));
 });
 
 $('#othertoggle').click(function() {
-	$('.course').slideUp();
-	$('.work').slideUp();
-	$('.volunteer').slideDown();
+	disableVisibility($(".course,.work"));
+	enableVisibility($(".volunteer"));
+	//$('.course').slideUp();
+	//$('.work').slideUp();
+	//$('.volunteer').slideDown();
+	updateVisiblity($(".course,.work,.volunteer"));
 });
+
+function disableVisibility(elements) {
+    //console.log("resetting visibility");
+    elements.data("filtered", true);
+    return;
+}
+
+function enableVisibility(elements) {
+    elements.data("filtered", false);
+    return;
+}
+
+$(document).ready(function() {
+	var expItems = $(".work,.course,.volunteer");
+
+	enableVisibility(expItems);
+});
+
+function updateVisiblity(elements) {
+    elements.each(function () {
+        if ($(this).data("filtered") == true) {
+            $(this).slideUp();
+        }
+        else {
+            $(this).slideDown();
+        }
+    });
+    return;
+}
